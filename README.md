38c8765341829208f8fab5fbfc1a61b33da8bfe0
elidianaandrade <lide_andrade_redes@hotmail.com>
2022-04-13 11:27:00 -0300

    light mode config iniciais

---
+++ README.md
@@ -0,0 +1,26 @@
+<div align="center">
+  <a href="https://elidianaandrade.github.io/archstil/">
+    <img alt="ArchStil" height="60" src="https://raw.githubusercontent.com/elidianaandrade/archstil-netflix-clone/main/assets/img/archstil-black-logo.png">
+  </a>
+</div>
+<br>
+<div align="center">
+  <a href="https://elidianaandrade.github.io/archstil/">
+    <img alt="Eli Cosmaker" src="https://github.com/elidianaandrade/archstil-netflix-clone/blob/main/assets/img/elicosmaker-screen.png?raw=true">
+  </a>
+</div>
+<br>
+
+# ELIDIANA ANDRADE | Portfolio
+
+## ⚔ About the project
+Recreating [Netflix's](https://www.netflix.com/) initial interface, where I bring my version about Architectural Styles. This project was developed for learning purposes in the course of the [Digital Innovation One](https://www.dio.me/).
+
+## 💻 Technologies
+- HTML5
+- CSS3
+- JavaScript
+
+## 🛠️ Utilities
+- **Icons:** [Font Awesome](https://fontawesome.com/icons)
+- **Favicon:** [favicon.ico generator](https://www.favicon.cc/)
--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-const toggleButton = document .getElementById('toggle-button');
-
+/* TOGGLE MENU  */
 function toggleMenu() {
-    const toggleNav = document .getElementById('toggle-nav');
+    const toggleNav = document.getElementById('toggle-nav');
     toggleNav.classList.toggle('active');
 }
 
+const toggleButton = document.getElementById('toggle-button');
+
 toggleButton.addEventListener('click', toggleMenu);
+/* TOGGLE MENU END */
 
 
+/* LIGHT MODE */
+function changeMode() {
+	changeClasses();
+}
+ 
+function changeClasses() {
+    body.classList.toggle(lightModeClass);
+    /* Light Text */
+    introductionTitle.classList.toggle(lightModeClass);
+    introductionDescription.classList.toggle(lightModeClass);
+}
+
+const lightModeClass = 'light-mode';
+const button = document.getElementById('mode-selector');
+const body = document.getElementsByTagName('body')[0];
+/* Light Text */
+const introductionTitle = document.getElementsByClassName('introduction-title')[0];
+const introductionDescription = document.getElementsByClassName('introduction-description')[0];
+
+button.addEventListener('click', changeMode);
+/* LIGHT END */
 
